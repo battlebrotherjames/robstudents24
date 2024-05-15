760163fd4f80e39146be79e06c17f96ba3082eba
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-05-15 14:36:13 -0600

    Fixed file_setup script

--- code/week2/file_setup.sh
+++ code/week2/file_setup.sh
@@ -1,3 +1,4 @@
-cd Desktop/robstudents24/
-mkdir week{2..10}
-mkdir final_project
+mkdir /home/student/Desktop/itm111/
+cd /home/student/Desktop/itm111/
+mkdir /home/student/Desktop/itm111/week{2..10}
+mkdir /home/student/Desktop/itm111/final_project
